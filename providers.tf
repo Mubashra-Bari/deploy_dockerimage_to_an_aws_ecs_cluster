b15f683b47bc0f5e248b5f4de7131183c1fdd110
Mubashra-Bari <101508312+Mubashra-Bari@users.noreply.github.com>
2022-07-04 22:14:57 +0530

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+provider "aws" {
+  region = var.region
+  access_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  secret_key = "jdosdionzjiomnoakd/dslijosk398267sdfkjo"
+}
